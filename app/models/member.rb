3470a9f9f92cd19506651aee14e004e6ea7d35a6
lucas-perata <contacto.perata@gmail.com>
2022-05-13 23:06:23 -0300

    Formulario edicion usuario en Bulma. Agregado de eliminacion automatica si un usuario elimina su cuenta de sus owls

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,8 @@
 class Member < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :owls 
+  has_many :owls, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
   
