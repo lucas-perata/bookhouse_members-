8b8cc7fca37e7fb3df144c3d592d2462eb92ec42
lucas-perata <contacto.perata@gmail.com>
2022-05-15 02:04:31 -0300

    Perfil autentificacion

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,7 +1,13 @@
 class MembersController < ApplicationController
+
+    before_action :authenticate_member!
+
     def profile 
         
         @members = current_member
         @owls = Owl.all.order("created_at DESC")
+
+        
+
     end 
 end
